03c80b8a0de5b483cab37be5ced44c88048f9a45
Roni Shaham <ronish31@gmail.com>
2020-01-02 11:02:17 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    'rules': {
+        "no-underscore-dangle": 0,
+        "max-len": [1, {
+            code: 250
+        }],
+        "no-param-reassign": 0,
+    },
+    'env': {
+        node: true,
+        jest: true,
+        es6: true,
+    },
+    'extends': [
+        'airbnb-base',
+    ],
+};
